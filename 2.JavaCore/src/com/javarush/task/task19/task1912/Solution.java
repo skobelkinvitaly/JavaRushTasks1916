60280105dbbad2189c8df8a9697e29ad0c8a6b8c
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2020-09-23 21:54:23 +0300

    Задача решена

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        String res = "";
+        String[] arr = result.split("te");
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0)
+                res = res + "??" + arr[i];
+            else res = res + arr[i];
+        }
+        System.setOut(consoleStream);
+        System.out.println(res);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
