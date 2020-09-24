13eb86e22b68aaa493018fa6fd839b29dc03796d
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2020-09-24 21:33:36 +0300

    Задача решена

--- 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -1,15 +1,42 @@
 package com.javarush.task.task19.task1923;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 /* 
 Слова с цифрами
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String file1 = args[0];
+        String file2 = args[1];
+        BufferedReader r = new BufferedReader(new FileReader(file1));
+        BufferedWriter w = new BufferedWriter(new FileWriter(file2));
+        while (true) {
+            int count = 0;
+            String temp = r.readLine();
+            if (temp == null) break;
+            else {
+                String [] array = temp.split(" ");
+//                ArrayList<String> stringArrayList = new ArrayList<>();
+//                stringArrayList = temp.split(" ");
+                for (int i = 0; i < array.length; i++) {
+                    char[ ] chars = array[i].toCharArray();
+                    for (int j = 0; j < chars.length; j++) {
+                        if (Character.isDigit(chars[j])) {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count ==1) {
+                        w.write(array[i] + " ");
+                        count = 0;
+                    }
+                }
+            }
+        }
+        r.close();
+        w.close();
     }
 }
