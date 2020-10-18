a346e849a3b735d46fba9bffe80811da1e4c8322
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2020-10-18 14:35:30 +0300

    Задача решена

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -1,51 +1,37 @@
 package com.javarush.task.task19.task1918;
 
-/* 
-Знакомство с тегами
-*/
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+/* 
+Знакомство с тегами
+*/
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        String filename = "";
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        filename = reader.readLine();
-        reader.close();
-        FileReader fileReader = new FileReader(filename);
-        String result = "";
-        ArrayList<String> array = new ArrayList<>();
-        while (fileReader.ready()) {
-            int data = fileReader.read();
-            result = result + (char) data;
-        }
-        fileReader.close();
-        String tagFinish = "</" + args[0] + ">";
-        String tagStart =  "<" + args[0] + " ";
-        String tagStart2 =  "<" + args[0] + ">";
-        int countStart = 0;
-        int countFinish = 0;
 
-        String[] sArray = result.split(tagFinish);
-        ArrayList<String> tags = new ArrayList<>();
-        int num = 0;
-        for (int i = 0; i < sArray.length; i++) {
-            sArray[i] = sArray[i].trim();
-            sArray[i] = sArray[i] + tagFinish;
-            sArray[i] = sArray[i].substring(sArray[i].indexOf("<"), sArray[i].lastIndexOf(">")+1);
-            System.out.println(sArray[i]);
-            char[] tagStringChars = sArray[i].toCharArray();
-            String s = "";
-            for (int j = 0; j < tagStringChars.length; j++) {
-                s = s + tagStringChars[i];
+        BufferedReader nameReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader = new BufferedReader(new FileReader(nameReader.readLine()));
+        nameReader.close();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (fileReader.ready()) stringBuilder.append(fileReader.readLine());
+        fileReader.close();
+        String text = String.valueOf(stringBuilder);                     // Строка, с которой работаем
+        String openTag = "<" + args[0], closeTag = "</" + args[0] + ">"; // Для удобства, сохраняем теги в переменные
+        LinkedList<Integer> queue = new LinkedList<>();                  // Очередь для ожидающих закрытия тегов, запоминаем все открытые
+        LinkedList<String> tempQueue = new LinkedList<>();               // Очередь для ожидающих вывода строк (очередь-2)
+        for (int i = 0; i <= text.length() - closeTag.length(); i++) {    // Проходим по тексту
+            if (text.substring(i).startsWith(openTag))
+                queue.addFirst(i);             // Записываем индекс открывающегося тега в очередь первым элементом
+            if (text.substring(i).startsWith(closeTag)) {                              // Если встретили закрывающися тег, то:
+                tempQueue.add(text.substring(queue.removeFirst(), i + closeTag.length()));      // Берем из очереди последний открывающийся тег и заносим в очередь-2
+                if (queue.isEmpty()) while (!tempQueue.isEmpty())
+                    System.out.println(tempQueue.removeLast()); // Если в очереди тегов закончилисm открывающиеся, начинаем выводить на печать из очереди-2
             }
         }
-
-
-
     }
 }
