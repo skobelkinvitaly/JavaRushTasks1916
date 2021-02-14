86338014731eeca0bd40e15ee6241c1cb9cb0f6b
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-02-14 21:30:48 +0300

    Задача решена

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Location.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task23.task2309.vo;
+
+import com.javarush.task.task23.task2309.AbstractDbSelectExecutor;
+
+import java.util.List;
+
+public class Location extends  NamedItem {
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/User.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task23.task2309.vo;
+import com.javarush.task.task23.task2309.*;
+
+import java.util.List;
+
+public class User extends NamedItem {
+
+}
