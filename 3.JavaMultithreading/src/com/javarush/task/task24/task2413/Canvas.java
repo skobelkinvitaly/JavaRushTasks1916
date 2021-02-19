de73814a63f9b75bc2fe8cfe20e5d659277bf997
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-02-19 21:45:47 +0300

    Задача решена

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,37 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+    private int width, height;
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    private char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height+2][width+2];
+    }
 }
