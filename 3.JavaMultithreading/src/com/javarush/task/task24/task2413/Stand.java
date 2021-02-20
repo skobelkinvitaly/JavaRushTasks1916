6100d86c241aa6a3a856c7e059f2d8fee6c30210
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-02-20 22:37:04 +0300

    Задача решена

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,6 +1,23 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double speed;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    private double direction;
     @Override
     void draw(Canvas canvas) {
 
