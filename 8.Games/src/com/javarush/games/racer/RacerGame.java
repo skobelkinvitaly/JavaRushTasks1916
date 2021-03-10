e0a5f7ba5c6b7fa1e7e09f475bdd8b8d70a196a7
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-03-10 23:29:07 +0300

    Задача решена

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -5,11 +5,26 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH/2;
+    public static final int ROADSIDE_WIDTH = 14;
+
+    private void drawField(){
+
+    }
+
+    private void createGame(){
+        drawScene();
+    };
+    private void drawScene(){
+        drawField();
+    };
+
 
     @Override
     public void initialize() {
         super.initialize();
         showGrid(false);
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
     }
 }
