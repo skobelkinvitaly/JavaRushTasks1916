044b8bd8253131333bb198b64d260a08954d988f
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-03-11 06:55:03 +0300

    Задача решена

---
+++ 8.Games/src/com/javarush/games/racer/GameObject.java
@@ -0,0 +1,27 @@
+package com.javarush.games.racer;
+
+import com.javarush.engine.cell.*;
+
+public class GameObject {
+    public int x;
+    public int y;
+    public int width;
+
+    public GameObject(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+        width = matrix[0].length;
+        height = matrix.length;
+    }
+
+    public int height;
+    public int[][] matrix;
+    public  void draw(Game game){
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                game.setCellColor(this.x + x,this.y + y, Color.values()[matrix[x][y]]);
+            }
+        }
+    }
+}
