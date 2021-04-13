b8fcb77da618478c54b1973be8627fab7697c861
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-13 21:36:29 +0300

    Задача выполнена.

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,4 +3,11 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    @Override
+    public void initialize() {
+//        super.initialize();
+        setScreenSize(SIDE, SIDE);
+    }
+
+    private  static final int SIDE = 9;
 }
