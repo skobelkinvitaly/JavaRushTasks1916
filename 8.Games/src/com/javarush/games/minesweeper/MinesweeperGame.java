7ad60e4f6b598845e525e48f525ba28ab65332d3
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-14 21:44:47 +0300

    Задача выполнена.

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -3,11 +3,22 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+
+
     @Override
     public void initialize() {
-//        super.initialize();
         setScreenSize(SIDE, SIDE);
+        createGame();
     }
 
-    private  static final int SIDE = 9;
+    private void createGame() {
+        for (int x = 0; x < SIDE; x++) {
+            for (int y = 0; y < SIDE; y++) {
+                gameField[y][x] = new GameObject(x, y);
+                setCellColor(x,y,Color.ORANGE);
+            }
+        }
+    }
 }
