a18fbba07b743fa4df5b98f165e563b88d71a155
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-14 22:06:31 +0300

    Задача выполнена.

--- 8.Games/src/com/javarush/games/minesweeper/GameObject.java
+++ 8.Games/src/com/javarush/games/minesweeper/GameObject.java
@@ -2,9 +2,12 @@ package com.javarush.games.minesweeper;
 
 public class GameObject {
     public int x, y;
+    public boolean isMine;
 
-    public GameObject(int x, int y) {
+    public GameObject(int x, int y, boolean isMine) {
         this.x = x;
         this.y = y;
+        this.isMine = isMine;
     }
+
 }
--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,19 +5,29 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
-
+    private int countMinesOnField;
 
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+//        countMinesOnField =
     }
 
     private void createGame() {
+//        countMinesOnField = getRandomNumber(10);
+        boolean bool;
+        int mines = 0;
         for (int x = 0; x < SIDE; x++) {
             for (int y = 0; y < SIDE; y++) {
-                gameField[y][x] = new GameObject(x, y);
-                setCellColor(x,y,Color.ORANGE);
+//                bool = gameField[x][y].isMine;
+                if (getRandomNumber(10) == 1) {
+                    bool = true;
+                    countMinesOnField++;
+                }
+                else bool = false;
+                gameField[y][x] = new GameObject(x, y, bool);
+                setCellColor(x, y, Color.ORANGE);
             }
         }
     }
