95ea1ce540a3107f73751efe03fa4a5053496ca8
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-20 21:45:05 +0300

    Задача выполнена.

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -21,7 +21,7 @@ public class Game2048 extends Game {
     private void drawScene() {
         for (int x = 0; x < SIDE; x++) {
             for (int y = 0; y < SIDE; y++) {
-                setCellColor(x, y, Color.GREEN);
+                setCellColoredNumber(x,y,gameField[y][x]);
             }
         }
     }
@@ -37,10 +37,26 @@ public class Game2048 extends Game {
                 break;
             }
         }
-//        for (int x = 0; x < SIDE; x++) {
-//            for (int y = 0; y < SIDE; y++) {
-//
-//            }
-//        }
+    }
+    private  Color getColorByValue(int value){
+        Color color = null;
+        if (value == 2) color = Color.DEEPSKYBLUE;
+        if (value == 4) color = Color.DEEPPINK;
+        if (value == 8) color = Color.DIMGRAY;
+        if (value == 16) color = Color.YELLOWGREEN;
+        if (value == 32) color = Color.DARKORANGE;
+        if (value == 64) color = Color.GRAY;
+        if (value == 128) color = Color.RED;
+        if (value == 256) color = Color.ROYALBLUE;
+        if (value == 512) color = Color.GREENYELLOW;
+        if (value == 1024) color = Color.BEIGE;
+        if (value == 2048) color = Color.YELLOW;
+        return color;
+    }
+    private void setCellColoredNumber(int x, int y, int value){
+        Color color2 = getColorByValue(value);
+        String num = "";
+        if (value !=0) num = String.valueOf(value);
+        setCellValueEx(x,y,color2, num);
     }
 }
