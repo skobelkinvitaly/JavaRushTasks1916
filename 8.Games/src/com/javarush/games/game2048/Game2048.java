2e0f1d2d1dc64d58e08b8020be9c3af820d44a68
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-20 20:26:29 +0300

    Задача выполнена.

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,21 +2,45 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
-public class Game2048 extends Game{
+public class Game2048 extends Game {
     private static final int SIDE = 4;
     private int[][] gameField = new int[SIDE][SIDE];
+
     @Override
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
         drawScene();
     }
-    private void createGame(){};
-    private void drawScene(){
+
+    private void createGame() {
+        createNewNumber();
+        createNewNumber();
+    }
+
+    private void drawScene() {
         for (int x = 0; x < SIDE; x++) {
             for (int y = 0; y < SIDE; y++) {
-                setCellColor(x,y,Color.GREEN);
+                setCellColor(x, y, Color.GREEN);
             }
         }
-    };
+    }
+
+    private void createNewNumber() {
+        while (true) {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+            if (gameField[x][y] == 0) {
+                int random = getRandomNumber(10);
+                if (random == 9) gameField[x][y] = 4;
+                else gameField[x][y] = 2;
+                break;
+            }
+        }
+//        for (int x = 0; x < SIDE; x++) {
+//            for (int y = 0; y < SIDE; y++) {
+//
+//            }
+//        }
+    }
 }
