88f45a77bf3a845e097ca0e3fd465e4b2f4b3687
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-04-23 21:57:56 +0300

    Задача выполнена.

---
+++ 8.Games/src/com/javarush/games/moonlander/GameObject.java
@@ -0,0 +1,10 @@
+package com.javarush.games.moonlander;
+
+public class GameObject {
+    public double x,y;
+
+    public GameObject(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
