3fff668e5fcdff32a66f3b701ea5f8547ece529f
SkobelkinVitaly <skobelkin_vitaly@mail.ru>
2021-05-26 22:55:27 +0300

    Задача выполнена.

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Human implements Alive{
     private List<Human> children = new ArrayList<>();
-    public static int nextId = 0;
+    private static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
@@ -72,9 +72,9 @@ public class Human implements Alive{
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 
     public void printSize() {
         System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
